787ef166e50568c7e4b3c15f10fefa9f9a0710da
Morten Tangen <morten.tangen@gmail.com>
2013-05-21 14:26:11 +0200

    nytt repo

---
+++ src/main/scala/cibrary/repository/BokRepository.scala
@@ -0,0 +1,14 @@
+package cibrary.repository
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: MorTan
+ * Date: 21.05.13
+ * Time: 14:17
+ * To change this template use File | Settings | File Templates.
+ */
+class BokRepository {
+
+
+
+}
