a30c460109645bfe46464031e97fb449792fb1a4
Peter Breunig <peteabre@gmail.com>
2013-05-21 15:26:01 +0200

    Person ting og tang.

---
+++ src/test/scala/cibrary/repository/PersonRepositoryTest.scala
@@ -0,0 +1,14 @@
+package cibrary.repository
+
+import org.scalatest.FunSpec
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Peter Breunig
+ * Date: 21.05.13
+ * Time: 15:22
+ * To change this template use File | Settings | File Templates.
+ */
+class PersonRepositoryTest extends FunSpec {
+
+}
