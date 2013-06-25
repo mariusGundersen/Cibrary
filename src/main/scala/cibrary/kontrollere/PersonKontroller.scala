5aca5840a30e93fff24670c40eba8386e9979274
oyvinvo <oyvind.volden@ciber.no>
2013-06-25 13:21:23 +0100

    IceCream

---
+++ src/main/scala/cibrary/kontrollere/PersonKontroller.scala
@@ -0,0 +1,12 @@
+package cibrary.kontrollere
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: oyvvol
+ * Date: 25.06.13
+ * Time: 13:45
+ * To change this template use File | Settings | File Templates.
+ */
+class PersonKontroller {
+
+}
---
+++ src/test/scala/cibrary/repository/PersonKontrollerTest.scala
@@ -0,0 +1,12 @@
+package cibrary.repository
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: oyvvol
+ * Date: 25.06.13
+ * Time: 13:54
+ * To change this template use File | Settings | File Templates.
+ */
+class PersonKontrollerTest {
+
+}
