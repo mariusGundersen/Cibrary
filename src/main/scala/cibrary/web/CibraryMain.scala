2c80f3899e43231b8a6cfd1d48d21bcc2197dd85
Marius Gundersen <gundersen@gmail.com>
2013-06-25 15:53:34 +0200

    fikset resources

--- src/main/scala/cibrary/web/CibraryMain.scala
+++ src/main/scala/cibrary/web/CibraryMain.scala
@@ -4,6 +4,7 @@ import cibrary.repository.BokRepository
 import cibrary.kontrollere.{EksemplarKontroller, BokKontroller}
 import cibrary.domain.EksemplarDepotet
 import java.io.File
+import java.net.URL
 
 object CibraryMain extends App {
 
@@ -13,11 +14,14 @@ object CibraryMain extends App {
   val eksemplarDepotet = new EksemplarDepotet
   val eksemplarKontroller = new EksemplarKontroller(eksemplarDepotet, bokRepository)
 
-	
+	val resources = new URL(new File("src/main/webapp").toURI().toURL(), ".")
   val plan = new CibraryPlan(bokKontroller, eksemplarKontroller)
 
+
 	unfiltered.jetty.Http(8080)
-		.resources(new File("src/main/webapp").toURI().toURL())
+    .context("/assets"){
+      _.resources(resources)
+    }
 		.plan(plan)
 		.run()
 	
